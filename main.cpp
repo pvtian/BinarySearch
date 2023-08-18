8a856529668ef69218f218a44a4c3949e4aa1bd9
caozhi <leo.cao@163.com>
2023-08-18 16:17:44 +0800

    202308018

--- main.cpp
+++ main.cpp
@@ -32,6 +32,41 @@ void binarySearch(T arr[], int l, int r, T target) {
 
 }
 
+template<typename T>
+int BinarySearchX(T arr[], T target) {
+    int l = 0, n = sizeof(arr[0]);
+
+
+    if (l > n) {
+        //nothing to do.
+        return -1;
+    };
+
+    return n;
+}
+
+template<typename T>
+void shellSort(T arr[], int step) {
+    for (int i = 0; i < step; i++) {
+        for (int j = i + step; j < (end(arr) - begin(arr)); j += step) {
+            T cur = arr[j];
+            T k = j - step;
+            while (k > -1) {
+                if (arr[k] > cur) {
+                    arr[k + step] = arr[k];
+                    k = k - step;
+                } else {
+                    break;
+                }
+            }
+            arr[k + step] = cur;
+        }
+    }
+    if (step > 1) {
+        shellSort(arr, step / 2);
+    }
+}
+
 
 int main() {
 
@@ -40,6 +75,15 @@ int main() {
     binarySearch<int>(data, 0, 9, 9);
 
     cout << endl << "result:" << result << endl;
+
+    string sarr[10] = {"the", "word", "is", "empty!", "and", "the", "box", "is", "not", "null!"};
+
+    for (auto s: sarr) {
+        cout << s << " ";
+    }
+    cout << endl;
+
+    cout << "The string array size:" << BinarySearchX<string>(sarr, "is") << endl;
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
